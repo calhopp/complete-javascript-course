7f2c85511b1b7bb19d88674859547be2b140595b
calhopp <calhopp@gmail.com>
2020-09-02 10:59:58 -0500

    Section 3 done

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,29 +1,43 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//functions
+/**calculateAge(1965);
 
+function calculateAge(year) {
+    console.log(2016-year);
+}
 
+//retirement(1965) //!doesn't work!
 
+var retirement = function(year) {
+    console.log(65-(2016-year));
+}
 
+retirement(1965);
+*/
+//variables
+/** 
 
+console.log(age);            //undefined
+var age = 23;
+//console.log(age);
 
-
-
-
-
-
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);        //65
+    console.log(window.age); //23
+}
+foo();
+console.log(age);            //23
+*/
 ///////////////////////////////////////
 // Lecture: Scoping
 
 
 // First scoping example
 
-/*
+/**
 var a = 'Hello!';
 first();
 
@@ -67,11 +81,36 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//console.log(this);
+/**
+calculateAge(1985);
 
+function calculateAge(year) {
+    console.log(2016-year);
+    console.log(this);
+}
+*/
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(2016-this.yearOfBirth);
+        console.log(this);
 
+        function inner() {
+            console.log(this);
+        }
+        inner();
+    }
+}
 
+john.calculateAge();
 
+var mike = {
+    name: "mike",
+    yearOfBirth: 1984
+}
 
+mike.calculateAge = john.calculateAge;
 
-
-
+mike.calculateAge()
